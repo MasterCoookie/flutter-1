a3620c7f241a0584a0a40e7871f72a6d0c12c21e
Jan Kocurek <jas.kocurek@wp.pl>
2021-05-01 00:27:38 +0200

    idk what im doing

--- lib/main.dart
+++ lib/main.dart
@@ -22,28 +22,83 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.green[900],
       ),
-      body: Row(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  TextButton(
+                    onPressed: (){},
+                    child: Text("ZMITAC", style: TextStyle(color: Colors.amber),),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.green[900]),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(vertical: 25),
+                    child: Text('ZMITAC'),
+                    color: Colors.green[400],
+                  ),
+                  Text("Wykłady z fyzyki")],
+              ),
+              Column(
+                children: [
+                  TextButton(
+                    onPressed: (){},
+                    child: Text("ZMITAC", style: TextStyle(color: Colors.amber),),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.green[900]),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(vertical: 25),
+                    child: Text('ZMITAC'),
+                    color: Colors.green[400],
+                  ),
+                  Text("Wykłady z fyzyki")],
+              ),
+              Column(
+                children: [
+                  TextButton(
+                    onPressed: (){},
+                    child: Text("ZMITAC", style: TextStyle(color: Colors.amber),),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.green[900]),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(vertical: 25),
+                    child: Text('ZMITAC'),
+                    color: Colors.green[400],
+                  ),
+                  Text("Wykłady z fyzyki")],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text(
+                'CZŁOWIEK ZMITAC PIJE PIWO',
+                style: TextStyle(
+                  color: Colors.red[900],
+                  fontSize: 25,
+                  backgroundColor: Colors.yellow[800],
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
 
-          TextButton(
-              onPressed: (){},
-              child: Text("ZMITAC", style: TextStyle(color: Colors.amber),),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.green[900]),
-              )
-            ,
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.symmetric(vertical: 25),
-            child: Text('ZMITAC'),
-            color: Colors.green[400],
-          ),
-          Text("Wykłady z fyzyki")
+              ),],
+          )
+
         ],
       ),
+
       floatingActionButton: FloatingActionButton(
         child: Text('ZMITAC', style: TextStyle(fontSize: 10)),
         onPressed: () {},
